713a574d4385d4a055218629cf6190674bc42ec4
Chris Casey <chriscasey@Chriss-MacBook-Air.local>
2015-11-23 22:08:38 -0800

    adding bootstrap3

--- dd_app/analyzer.py
+++ dd_app/analyzer.py
@@ -9,7 +9,7 @@ def calculate_scores(entries):
 
 	lifetime_earning_scores = calculate_lifetime_earning_rank_scores(entry_dict)
 	for earning_score in lifetime_earning_scores:
-		entry_dict[earning_score[0]].score += earning_score[1]*4
+		entry_dict[earning_score[0]].score += earning_score[1]*4	
 	
 	win_perc_rank_scores = calculate_lifetime_winning_perc_rank_scores(entry_dict)
 	for win_perc_score in win_perc_rank_scores:
@@ -23,7 +23,7 @@ def calculate_scores(entries):
 def calculate_lifetime_winning_perc_rank_scores(entry_dict):
 	winning_perc_by_entry = []
 	for entry_num, entry in entry_dict.iteritems():
-		winning_perc_by_entry.append( (entry.id, entry.lifetime_win_perc) )
+		winning_perc_by_entry.append( (entry.entry_num, entry.lifetime_win_perc) )
 	winning_perc_by_entry.sort(key=lambda tup: tup[1], reverse=True)
 	return determine_rank_scores(winning_perc_by_entry)
 
@@ -34,7 +34,7 @@ def calculate_lifetime_winning_perc_rank_scores(entry_dict):
 def calculate_lifetime_earning_rank_scores(entries):
 	earnings_by_entry = []
 	for entry_num, entry in entries.iteritems():
-		earnings_by_entry.append( (entry.id, entry.avg_earnings) )
+		earnings_by_entry.append( (entry.entry_num, entry.avg_earnings) )
 	earnings_by_entry.sort(key=lambda tup: tup[1], reverse=True)
 	return determine_rank_scores(earnings_by_entry)
 
@@ -47,7 +47,7 @@ def calculate_bsf_rank_scores(entry_dict):
 	for entry_num, entry in entry_dict.iteritems():
 		bsf_average = calculate_bsf_average(entry)
 		entry.avg_speed = bsf_average
-		entry_averages.append( (entry.id, bsf_average) )
+		entry_averages.append( (entry.entry_num, entry.avg_speed) )
 	entry_averages.sort(key=lambda tup: tup[1], reverse=True)
 	return determine_rank_scores(entry_averages)
 	
@@ -80,7 +80,6 @@ def determine_rank_scores(items):
 			result.append( (item[0], curr_score) )
 			consecutive_dups = 0
 		prev = item[1]	
-
 	return result			
 
 

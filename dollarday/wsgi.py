ea4fbbd17f9d626efd6eaf08e70a5035f31113e7
Chris Casey <chriscasey@Chriss-MacBook-Air.local>
2015-12-12 17:14:28 -0800

    fixing wsgi file

--- dollarday/wsgi.py
+++ dollarday/wsgi.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 
 import os
 
-from django.core.wsgi import 
+from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dollarday.settings")
